372d5f2e2eb26a9ea077efe5fcad81619cddfc7e
dPro-123 <ghoshdeepratim752006@gmail.com>
2026-08-28 10:50:08 +0530

    Add application screenshots

--- app/app.py
+++ app/app.py
@@ -2,6 +2,13 @@
 from pathlib import Path
 import tempfile
 import os
+import sys
+
+PROJECT_ROOT = Path(__file__).resolve().parent.parent
+
+if str(PROJECT_ROOT) not in sys.path:
+    sys.path.insert(0, str(PROJECT_ROOT))
+
 
 
 import pandas as pd
